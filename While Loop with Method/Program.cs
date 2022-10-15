c86097f23fe4a85ed93869edbee5aa473c59dc10
NET <NET@DESKTOP-NSMUMFG>
2022-10-15 11:10:02 +0600

    Add project files.

---
+++ While Loop with Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_with_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+
+            Console.ReadKey();
+
+        }
+
+        public static void Print()
+        {
+
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.Write($" {i} \t");
+                i = i + 1;
+            }
+
+      
+
+        }
+    }
+}
